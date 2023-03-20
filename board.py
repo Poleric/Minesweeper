0d61151463cde4a0a6c84cda948f6e489a1e3dea
Poleric <nixonleongkb@gmail.com>
2023-03-20 23:30:46 +0800

    Refactoring Moved most of the game logic into separate class and more abstraction, making BoardGUI more of a GUI wrapper.

--- board.py
+++ board.py
@@ -12,10 +12,19 @@ CoordT = tuple[int, int]
 
 
 class Board:
-    def __init__(self, width: int, height: int):
-        self.width = width
-        self.height = height
-        self.board = np.zeros((height, width), dtype=np.int8)  # create a board with zeros
+    def __init__(self, rows: int, columns: int):
+        self.rows = rows
+        self.columns = columns
+        self.board = self.create_board()
+
+    def __repr__(self):
+        return repr(self.board)
+
+    def create_board(self) -> np.ndarray:
+        """Method that is always called to initialize a board
+        Define this when subclassing to produce different kind of board"""
+
+        return np.zeros((self.rows, self.columns), dtype=np.int8)  # create a board with zeros
 
     def get_adjacent_coords(self, coordinate: CoordT) -> Iterator[CoordT]:
         """Get the coordinates of row and column of adjacent cell given a coordinate."""
@@ -24,24 +33,24 @@ class Board:
             for dcol in range(-1, 2):  # -1, 0, 1
                 row, col = coordinate[0] + drow, coordinate[1] + dcol  # adjacent cell
 
-                if 0 <= row < self.height and 0 <= col < self.width and (drow, dcol) != (0, 0):
+                if 0 <= row < self.rows and 0 <= col < self.columns and (drow, dcol) != (0, 0):
                     yield row, col
 
     def coords_to_number(self, coordinate: CoordT) -> int:
         """Translate a 2d coordinate into a flatten index"""
 
-        return coordinate[0] * self.width + coordinate[1]
+        return coordinate[0] * self.columns + coordinate[1]
 
     def number_to_coords(self, number: int) -> CoordT:
         """Translate the index of the flatten list into a 2d coordinate """
 
-        return divmod(number, self.width)
+        return divmod(number, self.columns)
 
 
 class MinesweeperBoard(Board):
     MINES_EDGE_DETECTION_KERNEL = np.ones((3, 3), dtype=np.int8) * -1
 
-    def __init__(self, width: int, height: int, mines: int | float, *, starting_tile: CoordT = None, seed=None):
+    def __init__(self, rows: int, columns: int, mines: int | float, *, starting_tile: CoordT = None, seed=None):
         """Create a minesweeper board
 
         :param: width <int> - the width of the board
@@ -52,18 +61,18 @@ class MinesweeperBoard(Board):
         :param: seed <int> - the seed used for the rng
         """
 
-        super().__init__(width, height)
+        super().__init__(rows, columns)
         self.rng = np.random.default_rng(seed)
 
         if 0 < mines < 1:  # convert mine density to mines
-            self.mines = max(1, int(mines*self.width*self.height))  # min 1 mine
+            self.mines = max(1, int(mines * self.rows * self.columns))  # min 1 mine
         else:
             self.mines = mines
 
         assert isinstance(mines, int), "Mines can only be an integer, or a decimal number between 0 and 1 to represent mine density"  # make sure mines is an integer
 
         if not starting_tile:
-            starting_tile = (self.rng.integers(0, self.height-1), self.rng.integers(0, self.width-1))
+            starting_tile = (self.rng.integers(0, self.columns - 1), self.rng.integers(0, self.rows - 1))
         self.starting_tile: CoordT = starting_tile
 
         self.generate_board()
@@ -71,7 +80,7 @@ class MinesweeperBoard(Board):
     def get_randomized_coords_for_mines(self, n: int) -> Iterator[CoordT]:
         """Get randomized coordinates for mine placements, will exclude starting position and the adjacent tiles to it."""
 
-        cell_num = list(range(self.width * self.height))  # get position in 1d space
+        cell_num = list(range(self.rows * self.columns))  # get position in 1d space
 
         cell_num.remove(self.coords_to_number(self.starting_tile))  # exclude starting position
         for coord in self.get_adjacent_coords(self.starting_tile):  # exclude the tiles adjacent to the starting position
@@ -106,3 +115,144 @@ class MinesweeperBoard(Board):
         marked_tiles = convolve2d(self.board, self.MINES_EDGE_DETECTION_KERNEL, mode="same")
         # for spot that is mine, set -1, else keep
         self.board = np.where(mines_positions, -1, marked_tiles)
+
+    def get_all_mine_coords(self) -> Iterator[CoordT]:
+        return zip(*np.where(self.board == -1))
+
+
+class TileBoard(Board):
+    def __init__(self, rows: int, columns: int):
+        """Used to generate visual board for playing
+
+        Created to store tile states (open, flagged) when playing
+        self.board: store tile values as the game goes on
+        self.opened: tile is opened or not
+        self.flagged: tile is flagged or not
+        """
+
+        super().__init__(rows, columns)
+
+        # Create arrays with all 0, switch to 1 to denote truthy
+        self.opened: np.ndarray = self.create_bool_board()
+        self.flagged: np.ndarray = self.create_bool_board()
+
+    def create_bool_board(self) -> np.ndarray:
+        return np.zeros((self.rows, self.columns), dtype=bool)
+
+    def is_opened(self, coord: CoordT) -> bool:
+        return self.opened[coord]
+
+    def is_flagged(self, coord: CoordT) -> bool:
+        return self.flagged[coord]
+
+    def get_all_opened_coords(self) -> Iterator[CoordT]:
+        return zip(*np.where(self.opened))
+
+    def get_all_flagged_coords(self) -> Iterator[CoordT]:
+        return zip(*np.where(self.flagged))
+
+    def flag_tile(self, coord: CoordT) -> None:
+        self.flagged[coord] = True
+
+    def unflag_tile(self, coord: CoordT) -> None:
+        self.flagged[coord] = False
+
+
+class GameBoard(TileBoard):
+    """Add actual board alongside the display board and game logic."""
+    def __init__(self, rows: int, columns: int, mines: int):
+        super().__init__(rows, columns)
+
+        self.mines = mines
+        self.minesweeper: MinesweeperBoard | None = None
+
+    def start_game(self, coord: CoordT) -> None:
+        self.generate_minesweeper_board(coord)
+
+    def click_tile(self, coord: CoordT) -> None:
+        """When a tile is clicked, do stuff based on the tile state"""
+        if self.minesweeper is None:  # not initialised yet
+            self.start_game(coord)
+
+        if self.is_flagged(coord):
+            return
+
+        if not self.is_opened(coord):
+            self.open_tile(coord)
+
+            if self.board[coord] == 0:  # no mines around
+                self.cascade_tile(coord)
+        else:
+            if self.can_look_around(coord):
+                self.cascade_tile(coord)
+
+        if self.board[coord] == -1:  # pressed mine
+            self.game_over(coord)
+
+        if self.is_win():
+            self.win()
+
+    def game_over(self, coord: CoordT) -> None:
+        """Reveal all mines"""
+        mines = self.minesweeper.board == -1
+        self.board = np.where(mines, -1, self.board)
+
+    def win(self) -> None:
+        """Flag all mines"""
+        for mine_coord in self.minesweeper.get_all_mine_coords():
+            self.flag_tile(mine_coord)
+
+    def cascade_tile(self, coord) -> None:
+        for coord in self.get_adjacent_unopened_and_unflagged_coords(coord):
+            self.click_tile(coord)
+
+    def generate_minesweeper_board(self, starting_tile: CoordT, seed=None) -> None:
+        self.minesweeper = MinesweeperBoard(self.rows, self.columns, self.mines, starting_tile=starting_tile, seed=seed)
+
+    def is_win(self) -> bool:
+        """Check if the number of non opened cells is equal to number of mines"""
+        return np.count_nonzero(self.opened == 0) == self.mines
+
+    def open_tile(self, coord: CoordT) -> None:
+        """"Open" a tile and set its value to its respective tile type. Ideally called once per tile."""
+        self.board[coord] = self.minesweeper.board[coord]
+        self.opened[coord] = True
+
+    def get_adjacent_unopened_and_unflagged_coords(self, coord: CoordT) -> Iterator[CoordT]:
+        """Used to cascade tiles and "look around" tiles"""
+
+        for coord in self.get_adjacent_coords(coord):
+            if self.is_opened(coord) or self.is_flagged(coord):
+                continue
+            yield coord
+
+    def get_adjacent_flagged_coords(self, coord: CoordT) -> Iterator[CoordT]:
+        for coord in self.get_adjacent_coords(coord):
+            if self.is_flagged(coord):
+                yield coord
+
+    def can_look_around(self, coord: CoordT) -> bool:
+        """See if the number on the tile is less than the number of adjacent flags"""
+        return self.board[coord] <= len(tuple(self.get_adjacent_flagged_coords(coord)))
+
+    def get_wrong_flag_coords(self) -> Iterator[CoordT]:
+        mines = self.minesweeper.board == -1
+
+        # using an A AND NOT B logic gate
+        #  flag   mines   out
+        #   0      0       0
+        #   0      1       0
+        #   1      0       1
+        #   1      1       0
+        return zip(*np.where(self.flagged & ~mines))
+
+    def get_unflagged_mine_coords(self) -> Iterator[CoordT]:
+        mines = self.minesweeper.board == -1
+
+        # using a NOT A AND B logic gate
+        #  flag   mines   out
+        #   0      0       0
+        #   0      1       1
+        #   1      0       0
+        #   1      1       0
+        return zip(*np.where(~self.flagged & mines))
--- gui.py
+++ gui.py
@@ -1,45 +1,23 @@
 from tkinter import *
-from board import Board, MinesweeperBoard
+from board import GameBoard
 import numpy as np
-import sys
 from datetime import timedelta
 
-from dataclasses import dataclass
-
-from typing import Callable, Iterator, Literal
+import sys
+from typing import Callable
 
 LEFT_CLICK = "<Button-1>"
 RIGHT_CLICK = "<Button-2>" if sys.platform == 'darwin' else "<Button-3>"
 CoordT = tuple[int, int]
 
 
-@dataclass
-class Tile:
-    button: Button
-
-    value: int = None  # Values follows minesweeper board, eg -1: mines, 0: 0 mines adjacent. None means unopened
-    is_flagged: bool = False
-
-    @property
-    def is_opened(self) -> bool:
-        return self.value is not None
-
-
-class BoardGUI(Frame, Board):
-
-    def __init__(self, tk, width: int, height: int, mines: int, seed=None):
+class BoardGUI(Frame, GameBoard):
+    def __init__(self, tk, rows: int, columns: int, mines: int, seed=None):
         super().__init__(tk)
+        GameBoard.__init__(self, rows, columns, mines)
 
-        self.width = width
-        self.height = height
-
-        self.board: np.ndarray[Tile] = np.empty((height, width), dtype=object)
-
-        self.mines = mines
+        self.PLAYING = True
         self.seed = seed
-        self.minesweeper: MinesweeperBoard = None
-
-        self.PLAYING = True  # change to False if lose
 
         self.images = {
             "closed": PhotoImage(file="./img/closed.png"),
@@ -53,217 +31,133 @@ class BoardGUI(Frame, Board):
             ]
         }
 
-        self.time = 0
+        self.button_frame = Frame(self)
+        self.buttons = self.create_button_board(self.button_frame)
+
+        self.time = 0  # time in seconds
         self.timer = Label(self, text="0:00:00", font=("Arial", 12))
+
+        self.display()
+
+    def display(self) -> None:
+        """Display the board and the timer"""
+
         self.timer.pack()
+        self.button_frame.pack()
 
-        self.board_frame = Frame(self)
-        self.display_board()
+    def create_button_board(self, tk) -> np.ndarray:
+        ar = np.empty((self.rows, self.columns), dtype=object)
+        closed_image = self.images["closed"]
 
-    def display_board(self):
-        for row in range(self.height):
-            for col in range(self.width):
-                # Setup button
-                button = Button(self.board_frame, image=self.images["closed"], width=25, height=25)
-                button.bind(LEFT_CLICK, self._on_left_click_wrapper(row, col))
-                button.bind(RIGHT_CLICK, self._on_right_click_wrapper(row, col))
+        def left_click(row, col) -> Callable:
+            coord = (row, col)
+
+            def func(event):
+                if not self.PLAYING:
+                    return
+
+                self.click_tile(coord)
+
+            return func
+
+        def right_click(row, col) -> Callable:
+            coord = (row, col)
+
+            def func(event):
+                if not self.PLAYING or self.is_opened(coord):
+                    return
+
+                if not self.is_flagged(coord):
+                    self.flag_tile(coord)
+                else:
+                    self.unflag_tile(coord)
+
+            return func
+
+        for row in range(self.rows):
+            for col in range(self.columns):
+                button = Button(tk, image=closed_image, width=25, height=25)
+                button.bind(LEFT_CLICK, left_click(row, col))
+                button.bind(RIGHT_CLICK, right_click(row, col))
                 button.grid(row=row, column=col)
 
-                tile = Tile(button=button)
+                ar[row, col] = button
+        return ar
 
-                self.board[row, col] = tile
-        self.board_frame.pack()
+    def open_tile(self, coord: CoordT) -> None:
+        super().open_tile(coord)
 
-    def generate_minesweeper_board(self, starting_tile: CoordT):
-        self.minesweeper = MinesweeperBoard(self.width, self.height, self.mines, starting_tile=starting_tile, seed=self.seed)
-
-    def get_adjacent_flag_coords(self, coord: CoordT) -> Iterator[CoordT]:
-        """Get coordinates of surrounding flags"""
-
-        for coord in self.get_adjacent_coords(coord):
-            if self.board[coord].is_flagged:
-                yield coord
-
-    def get_cascadeable_cells(self, coord: CoordT) -> Iterator[CoordT]:
-        """Returns adjacent unopened cells coordinates to open / cascade"""
-
-        for coord in self.get_adjacent_coords(coord):
-            if not self.board[coord].is_opened:
-                yield coord
-
-    def can_look_around(self, coord: CoordT) -> bool:
-        """Check if numbered tiles can open adjacent tiles depending on number of flags. TODO: find the right terminology for this"""
-
-        tile: Tile = self.board[coord]
-        if not tile.is_opened:
-            return False
-
-        number_of_adj_flags = len(list(self.get_adjacent_flag_coords(coord)))
-        if (tile.value - number_of_adj_flags) <= 0:
-            return True
-
-        return False
-
-    def look_around(self, coord: CoordT) -> None:
-
-        adj_coords = self.get_adjacent_coords(coord)
-        adj_flagged = self.get_adjacent_flag_coords(coord)
-
-        adj_not_flagged = set(adj_coords) - set(adj_flagged)
-        for coord in adj_not_flagged:
-            if not self.board[coord].is_opened:
-                self.open_tile(coord)
-
-    def uncover_tile(self, tile: Tile, tile_type: int) -> None:
-        """Set tile value and image"""
-
-        tile.value = tile_type
-
-        if tile_type == -1:  # is mine
+        # set the picture
+        tile_type = self.board[coord]
+        if tile_type == -1:
             image = self.images["mine_red"]
         else:
             image = self.images["tile"][tile_type]
-        tile.button.configure(image=image)
+        self.buttons[coord].configure(image=image)
 
-    def flag_tile(self, tile: Tile) -> None:
-        tile.is_flagged = True
+    def start_game(self, coord: CoordT) -> None:
+        super().start_game(coord)
+        self.start_timer()
 
-        image = self.images["flag"]
-        tile.button.configure(image=image)
-
-    def unflag_tile(self, tile: Tile) -> None:
-        tile.is_flagged = False
-
-        image = self.images["closed"]
-        tile.button.configure(image=image)
-
-    def _on_left_click_wrapper(self, row, col) -> Callable:
-        return lambda _: self.on_left_click((row, col))
-
-    def _on_right_click_wrapper(self, row, col) -> Callable:
-        return lambda _: self.on_right_click((row, col))
-
-    def on_left_click(self, coord: CoordT):
-        if not self.PLAYING:
-            return
-
-        if not self.minesweeper:
-            self.generate_minesweeper_board(coord)
-            self.start_timer()
-
-        tile: Tile = self.board[coord]
-        if tile.is_flagged:
-            return
-
-        if not tile.is_opened:
-            self.open_tile(coord)
-        elif self.can_look_around(coord):
-            self.look_around(coord)
-
-        if self.check_win():
-            self.win()
-
-    def on_right_click(self, coord: CoordT):
-        if not self.PLAYING:
-            return
-
-        tile: Tile = self.board[coord]
-        if tile.is_opened:
-            return
-
-        if not tile.is_flagged:
-            self.flag_tile(tile)
-        else:
-            self.unflag_tile(tile)
-
-    def open_tile(self, coord: CoordT) -> None:
-        tile: Tile = self.board[coord]
-
-        self.uncover_tile(tile, tile_type=self.minesweeper.board[coord])
-        if tile.value == -1:  # is mine
-            self.game_over()
-            return
-
-        if tile.value == 0:
-            for coord in self.get_cascadeable_cells(coord):
-                self.open_tile(coord)
-
-    def game_over(self):
-        self.PLAYING = False
-        self.show_stray_mines()
-        self.show_wrong_flag()
-
-    def show_stray_mines(self):
-        """Show mines that isn't flagged"""
-
-        for coord in zip(*np.where(self.minesweeper.board == -1)):  # get all mine coords
-            tile: Tile = self.board[coord]
-
-            if tile.is_flagged or tile.is_opened:  # flagged / mine that was clicked on
-                continue
-
-            image = self.images["mine"]
-            tile.button.configure(image=image)
-
-    def show_wrong_flag(self):
-        """Show tiles that have been falsely flagged"""
-
-        is_flagged = np.vectorize(lambda t: t.is_flagged)
-        flags = is_flagged(self.board)
-
-        for coord in zip(*np.where(flags)):
-            if self.minesweeper.board[coord] != -1:
-                image = self.images["flag_wrong"]
-                self.board[coord].button.configure(image=image)
-
-    def flag_all_unflagged(self):
-        """Flag all unflagged tiles when win"""
-
-        is_unopened = np.vectorize(lambda t: not t.is_opened)
-        unopened = is_unopened(self.board)
-
-        for coord in zip(*np.where(unopened)):
-            self.flag_tile(self.board[coord])
-
-    def check_win(self) -> bool:
-        """Check if number of unopened tiles == number of mines"""
-
-        is_unopened = np.vectorize(lambda t: not t.is_opened)
-        count_unopened = np.count_nonzero(is_unopened(self.board))
-        return self.mines == count_unopened
-
-    def win(self):
-        self.PLAYING = False
-        self.flag_all_unflagged()
-
-    def start_timer(self):
-        def update():
+    def start_timer(self) -> None:
+        def update_timer():
             if self.PLAYING:
                 self.timer.configure(text=str(timedelta(seconds=self.time)))
                 self.time += 1
+                self.timer.after(1000, update_timer)
+        update_timer()
 
-                self.timer.after(1000, update)
+    def flag_tile(self, coord: CoordT) -> None:
+        super().flag_tile(coord)
+        self.buttons[coord].configure(image=self.images["flag"])
 
-        update()
+    def unflag_tile(self, coord: CoordT) -> None:
+        super().unflag_tile(coord)
+        self.buttons[coord].configure(image=self.images["closed"])
+
+    def win(self) -> None:
+        """Win game and flag all unflagged mines"""
+
+        self.PLAYING = False
+        for mine_coord in self.get_unflagged_mine_coords():
+            self.flag_tile(mine_coord)
+
+    def game_over(self, coord: CoordT) -> None:
+        """Lose game and show all other mines, also shows wrong flagged
+        :param coord: Coordinate of the clicked mines. Clicked mine have different image from revealed mines
+        """
+
+        self.PLAYING = False
+
+        red_mine_image = self.images["mine"]
+        for mine_coord in self.get_unflagged_mine_coords():
+            if mine_coord == coord:
+                continue
+
+            self.buttons[mine_coord].configure(image=red_mine_image)
+
+        wrong_flag_image = self.images["flag_wrong"]
+        for flag_coord in self.get_wrong_flag_coords():
+            self.buttons[flag_coord].configure(image=wrong_flag_image)
 
 
 class MainGUI(Frame):
     def __init__(self, tk):
         super().__init__(tk)
 
-        self.width_frame = Frame(self)
-        self.width = IntVar(value=10)
-        self.width_label = Label(self.width_frame, text="Width", font=("Arial", 10))
-        self.width_input = Entry(self.width_frame, textvariable=self.width, width=5)
-        self.width_label.grid(row=0, column=0)
-        self.width_input.grid(row=0, column=1)
+        self.rows_frame = Frame(self)
+        self.rows = IntVar(value=10)
+        self.rows_label = Label(self.rows_frame, text="Rows", font=("Arial", 10))
+        self.rows_input = Entry(self.rows_frame, textvariable=self.rows, width=5)
+        self.rows_label.grid(row=0, column=0)
+        self.rows_input.grid(row=0, column=1)
 
-        self.height_frame = Frame(self)
-        self.height = IntVar(value=10)
-        self.height_label = Label(self.height_frame, text="Height", font=("Arial", 10))
-        self.height_input = Entry(self.height_frame, textvariable=self.height, width=5)
-        self.height_label.grid(row=0, column=0)
-        self.height_input.grid(row=0, column=1)
+        self.columns_frame = Frame(self)
+        self.columns = IntVar(value=10)
+        self.columns_label = Label(self.columns_frame, text="Columns", font=("Arial", 10))
+        self.columns_input = Entry(self.columns_frame, textvariable=self.columns, width=5)
+        self.columns_label.grid(row=0, column=0)
+        self.columns_input.grid(row=0, column=1)
 
         self.mines_frame = Frame(self)
         self.mines = IntVar(value=10)
@@ -274,31 +168,31 @@ class MainGUI(Frame):
 
         self.set_button = Button(self, text="Set", command=self.load_board)
 
-        self.width_frame.grid(row=0, column=0)
-        self.height_frame.grid(row=0, column=1)
+        self.rows_frame.grid(row=0, column=0)
+        self.columns_frame.grid(row=0, column=1)
         self.mines_frame.grid(row=0, column=2)
         self.set_button.grid(row=0, column=3)
 
-        self.board: BoardGUI = None
+        self.board: BoardGUI | None = None
         self.load_board()
 
     # def set_mode(self, mode: Literal["Easy", "Intermediate", "Expert"]):
     #     match mode.casefold():
     #         case "easy":
-    #             self.width.set(10)
-    #             self.height.set(10)
+    #             self.rows.set(10)
+    #             self.columns.set(10)
     #             self.mines.set(10)
     #         case "intermediate":
-    #             self.width.set(16)
-    #             self.height.set(16)
+    #             self.rows.set(16)
+    #             self.columns.set(16)
     #             self.mines.set(40)
     #         case "expert":
-    #             self.width.set(30)
-    #             self.height.set(16)
+    #             self.rows.set(30)
+    #             self.columns.set(16)
     #             self.mines.set(99)
 
     def validate_mines(self):
-        area = self.width.get() * self.height.get()
+        area = self.rows.get() * self.columns.get()
         mine_density = self.mines.get() / area
         if mine_density < 0:
             self.mines.set(int(0.1*area))
@@ -310,7 +204,7 @@ class MainGUI(Frame):
             self.board.destroy()
 
         self.validate_mines()
-        self.board = BoardGUI(self, self.width.get(), self.height.get(), self.mines.get())
+        self.board = BoardGUI(self, self.rows.get(), self.columns.get(), self.mines.get())
         self.board.grid(row=1, columnspan=4)
         return self.board
 
